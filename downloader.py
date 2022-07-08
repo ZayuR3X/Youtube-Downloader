3e23116162ebb4ecf32c7391448237d6e5f9e5fb
ZayuR3X <ak.2004.turkey@gmail.com>
2022-07-08 23:29:20 +0300

    codes were written :)

---
+++ downloader.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+import os
+
+
+link = input("Link: ")
+
+directory = input("Directory: ")
+
+try: 
+    yt = YouTube(link) #link kontrol
+except:
+    print ("ınvalid Link !!!") #böyle bi link yok kanka
+    exit()
+
+mp3 = yt.streams.filter(only_audio=True).first()
+
+print ("Donwloading...")
+
+output = mp3.download(directory)
+
+base, ext = os.path.splitext(output)
+to_mp3 = base + ".mp3"
+os.rename(output, to_mp3)
+
+print ("Download Complate!")
